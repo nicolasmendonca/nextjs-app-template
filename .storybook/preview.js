6c1de0afa5e3cad982b0f087119ed00735b2b8c4
Niko Mendo <nicolasmendonca@gmail.com>
2021-03-14 14:56:14 -0300

    Adds storybook jest plugin

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,14 @@
+import { addDecorator } from '@storybook/react'; // <- or your view layer
+import { withTests } from '@storybook/addon-jest';
+
+import results from '../.jest-test-results.json';
+
+addDecorator(
+	withTests({
+		results,
+	})
+);
 
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
-}
+	actions: { argTypesRegex: '^on[A-Z].*' },
+};
